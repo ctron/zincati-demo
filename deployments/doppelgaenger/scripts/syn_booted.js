74b8278cf9f1fa95431b9891f795e80b5afa99da
Jens Reimann <jreimann@redhat.com>
2022-12-01 13:53:29 +0100

    fix: handle case when property is missing

--- deployments/doppelgaenger/scripts/syn_booted.js
+++ deployments/doppelgaenger/scripts/syn_booted.js
@@ -1,9 +1,14 @@
-
 function find(predicate) {
     return context.newState.reportedState?.["deployments"]?.value?.find?.(predicate);
 }
 
-const deployment = find(deployment => deployment.booted)?.version;
-const result = { version: deployment.version, checksum: deployment.checksum };
+const deployment = find(deployment => deployment.booted);
+
+let result;
+
+if (deployment !== null) {
+    result = {version: deployment.version, checksum: deployment.checksum};
+}
+
 
 result
--- deployments/doppelgaenger/scripts/syn_staged.js
+++ deployments/doppelgaenger/scripts/syn_staged.js
@@ -1,9 +1,14 @@
-
 function find(predicate) {
     return context.newState.reportedState?.["deployments"]?.value?.find?.(predicate);
 }
 
-const deployment = find(deployment => deployment.staged)?.version;
-const result = { version: deployment.version, checksum: deployment.checksum };
+const deployment = find(deployment => deployment.staged);
+
+let result;
+
+if (deployment !== null) {
+    result = {version: deployment.version, checksum: deployment.checksum};
+}
+
 
 result
